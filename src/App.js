d16a1b12e5c0c986ce475aa2ff778ef1b50dca07
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2022-12-25 16:38:52 +0200

    change the text of each key to string instead of <p> (to fix a problem in clicking)

--- src/App.js
+++ src/App.js
@@ -25,43 +25,43 @@ function App() {
           <div className='row'>
             <div className='drum-pad' id='Q-pad' onClick={playAudio}>
               <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className='clip' id='Q'></audio>
-              <p>Q</p>
+              Q
             </div>
             <div className='drum-pad' id='W-pad'>
               <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" className='clip' id='W'></audio>
-              <p>W</p>
+              W
             </div>
             <div className='drum-pad' id='E-pad'>
               <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" className='clip' id='E'></audio>
-              <p>E</p>
+              E
             </div>
           </div>
           <div className='row'>
             <div className='drum-pad' id='A-pad'>
               <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" className='clip' id='A'></audio>
-              <p>A</p>
+              A
             </div>
             <div className='drum-pad' id='S-pad'>
               <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" className='clip' id='S'></audio>
-              <p>S</p>
+              S
             </div>
             <div className='drum-pad' id='D-pad'>
               <audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" className='clip' id='D'></audio>
-              <p>D</p>
+              D
             </div>
           </div>
           <div className='row'>
             <div className='drum-pad' id='Z-pad'>
               <audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" className='clip' id='Z'></audio>
-              <p>Z</p>
+              Z
             </div>
             <div className='drum-pad' id='X-pad'>
               <audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" className='clip' id='X'></audio>
-              <p>X</p>
+              X
             </div>
             <div className='drum-pad' id='C-pad'>
               <audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" className='clip' id='C'></audio>
-              <p>C</p>
+              C
             </div>
           </div>
         </div>
